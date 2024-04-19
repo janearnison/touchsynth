77cef83f55b2b554c01c13d5a38f8092b4cbc56f
janearnison <94143682+janearnison@users.noreply.github.com>
2024-04-19 10:16:25 +0200

    max 4 touches

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ function draw() {
   fill('magenta');
 
   // for each touch, draw an ellipse at its location with a unique color.
-  for (var i = 0; i < touches.length; i++) {
+  for (var i = 0; i < Math.min(touches.length, 4); i++) {
     // Create a unique color based on the touch index
     var uniqueColor = color((i * 30) % 255, (i * 40) % 255, (i * 50) % 255);
     fill(uniqueColor);
     ellipse(touches[i].x, touches[i].y, 50, 50);
-  }
 }
-
+}
 // do this prevent default touch interaction
 function mousePressed() {
   return false;
